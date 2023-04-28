2977ce6ce327a1bd81709c375b394f1a3fa2182c
AgnoDev <agnodev87@gmail.com>
2023-04-28 15:22:44 -0300

    implementa ClienteRequest e ClienteResponse

--- src/main/java/br/com/paidepet/petshop/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/paidepet/petshop/cliente/application/api/ClienteRequest.java
@@ -1,4 +1,36 @@
 package br.com.paidepet.petshop.cliente.application.api;
 
+import br.com.paidepet.petshop.cliente.domain.Sexo;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+
+@Value
 public class ClienteRequest {
+
+
+    @NotBlank
+    private String nomeCompleto;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String celular;
+    private String telefone;
+    private Sexo sexo;
+
+    @NotNull
+    private LocalDate dataNascimento;
+
+    @CPF
+    private String cpf;
+
+    @NotNull
+    private boolean aceitaTermos;
 }
--- src/main/java/br/com/paidepet/petshop/cliente/application/api/ClienteResponse.java
+++ src/main/java/br/com/paidepet/petshop/cliente/application/api/ClienteResponse.java
@@ -1,4 +1,12 @@
 package br.com.paidepet.petshop.cliente.application.api;
 
+import lombok.Value;
+
+import java.util.UUID;
+
+@Value
 public class ClienteResponse {
+    private UUID idCliente;
+
+
 }
