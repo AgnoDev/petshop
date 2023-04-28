d06bf82b658da854d8512e19543906b6f0105e70
AgnoDev <agnodev87@gmail.com>
2023-04-28 15:15:42 -0300

    implementa Controller e fluxo para Service

--- src/main/java/br/com/paidepet/petshop/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/paidepet/petshop/cliente/application/api/ClienteController.java
@@ -1,15 +1,30 @@
 package br.com.paidepet.petshop.cliente.application.api;
 
+import br.com.paidepet.petshop.cliente.application.service.ClienteService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Log4j2
+@RequiredArgsConstructor    //gera um construtor para o ClienteService só com atributos 'final'.
+                            //diminuindo a verbosidade do codigo
 public class ClienteController implements ClienteAPI {
+    private final ClienteService clienteService;    //INJEÇÃO DE DEPENDÊNCIA (final não pode ser modificado)
+   /* private ClienteController(ClienteService clienteService) {  //CONSTRUTOR que inicia o ClienteService
+        this.clienteService = clienteService; //porém ele deve ser substituído pelo @RequiredArgsConstructor
+    }*/
     @Override
     public ClienteResponse postCliente(ClienteRequest clienteRequest) {
         log.info("[start] ClienteController - postCliente");
+        ClienteResponse clienteCriado = clienteService.criaCliente(clienteRequest);
         log.info("[finish] ClienteController - postCliente");
-        return null;
+        return clienteCriado;
+        //"clienteService vai criaCliente baseado no clienteRequest passado na requisição do front"
+        //"e devolve um ClienteResponse chamado clienteCriado passando essa resposta no return"
+
+        
+
+
     }
 }
---
+++ src/main/java/br/com/paidepet/petshop/cliente/application/service/ClienteService.java
@@ -0,0 +1,8 @@
+package br.com.paidepet.petshop.cliente.application.service;
+
+import br.com.paidepet.petshop.cliente.application.api.ClienteRequest;
+import br.com.paidepet.petshop.cliente.application.api.ClienteResponse;
+
+public interface ClienteService {
+    ClienteResponse criaCliente(ClienteRequest clienteRequest);
+}
