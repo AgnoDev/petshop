92a10dd7471bdd5e3cd29f414172149bf8cd3b2c
AgnoDev <agnodev87@gmail.com>
2023-04-29 12:42:25 -0300

    implementa camada de Infra Repository

---
+++ src/main/java/br/com/paidepet/petshop/cliente/infra/ClienteInfraRepository.java
@@ -0,0 +1,18 @@
+package br.com.paidepet.petshop.cliente.infra;
+
+import br.com.paidepet.petshop.cliente.application.repository.ClienteRepository;
+import br.com.paidepet.petshop.cliente.domain.Cliente;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Log4j2
+public class ClienteInfraRepository implements ClienteRepository {
+    @Override
+    public Cliente salva(Cliente cliente) {
+        log.info("[start] ClienteInfraRepository - salva");
+
+        log.info("[finish] ClienteInfraRepository - salva");
+        return cliente;
+    }
+}
