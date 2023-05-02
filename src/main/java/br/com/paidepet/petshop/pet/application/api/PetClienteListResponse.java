cabbb1a99c6589db60e8727b42ad3195ef743f72
AgnoDev <agnodev87@gmail.com>
2023-05-02 15:19:20 -0300

    implementa PetClienteListResponse

--- src/main/java/br/com/paidepet/petshop/pet/application/api/PetClienteListResponse.java
+++ src/main/java/br/com/paidepet/petshop/pet/application/api/PetClienteListResponse.java
@@ -1,12 +1,42 @@
 package br.com.paidepet.petshop.pet.application.api;
 
 import br.com.paidepet.petshop.pet.domain.Pet;
+import br.com.paidepet.petshop.pet.domain.Porte;
+import br.com.paidepet.petshop.pet.domain.SexoPet;
+import br.com.paidepet.petshop.pet.domain.TipoPet;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
+@Value
 public class PetClienteListResponse {
+    private UUID idPet;
+    private String nomePet;
+    private TipoPet tipoPet;
+    private String raca;
+    private String pelagemCor;
+    private LocalDate dataNascimento;
+
     public static List<PetClienteListResponse> converter(List<Pet> getPetsByCliente) {
-    return null;
+        return getPetsByCliente.stream()
+                .map(PetClienteListResponse::new)
+                .collect(Collectors.toList());
+    }
+    public PetClienteListResponse(Pet pet){
+        this.idPet = pet.getIdPet();
+        this.nomePet = pet.getNomePet();
+        this.tipoPet = pet.getTipoPet();
+        this.raca = pet.getRaca();
+        this.pelagemCor = pet.getPelagemCor();
+        this.dataNascimento = pet.getDataNascimento();
     }
 }
 
